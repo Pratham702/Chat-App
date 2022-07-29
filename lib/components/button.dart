0aac37097d548c309ef9937f844370ce57737ecd
Pratham <20145@iiitu.ac.in>
2022-07-30 01:33:40 +0530

    initial commit

---
+++ lib/components/button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+class button extends StatelessWidget {
+  button({this.text,this.colour,this.navigator});
+  final String text;
+  final Color colour;
+  final Function navigator;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        elevation: 5.0,
+        color: colour,
+        borderRadius: BorderRadius.circular(30.0),
+        child: MaterialButton(
+          onPressed: navigator,
+          minWidth: 200.0,
+          height: 42.0,
+          child: Text(
+            text,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
